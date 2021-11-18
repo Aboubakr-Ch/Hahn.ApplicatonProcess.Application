702f62835e6e2f7d71e89d33d825bbb991db820f
StoreTEK <StoreTEK@LAPTOP-CHVQG4N3>
2021-11-18 19:52:55 +0100

    Add project files.

---
+++ Hahn.ApplicatonProcess.July2021.Domain/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Hahn.ApplicatonProcess.July2021.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hahn.ApplicatonProcess.July2021.Domain
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Asset> Posts { get; set; }
+    }
+}
